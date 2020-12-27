06afbcfd1e9bce11e743338d91a1d55971d19018
xiaofei <43567878+xiaofei0722@users.noreply.github.com>
2020-12-27 16:48:58 +0800

    orm

--- projects/models.py
+++ projects/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+
+class Projects(models.Model):
+    name = models.CharField(max_length=200,verbose_name='项目名称',unique=True,help_text='')
