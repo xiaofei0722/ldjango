097ac479dfb646332d995970e176b5decff9fdb7
xiaofei <43567878+xiaofei0722@users.noreply.github.com>
2021-01-01 14:21:00 +0800

    orm

--- projects/views.py
+++ projects/views.py
@@ -2,9 +2,11 @@ import json
 
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
-
+from django.db.models import Q
 # Create your views here.
 from django.views import View
+
+from interfaces.models import Interface
 from projects.models import Projects
 
 # def haha(request):
@@ -29,8 +31,26 @@ class IndexView(View):
         # one_obj = Projects.objects.get(id=1).leader
         # one_obj = Projects.objects.filter(desc__contains='简要')
 
-        one_obj = Projects.objects.filter(interface__name='登录接口')
+        # one_obj = Projects.objects.filter(interface__name='登录接口')
+        # one_obj = Interface.objects.filter(project__name='接口自动化')
 
+        # one_obj = Projects.objects.filter(id__gt=2)
+
+        # one_obj = Projects.objects.filter(id=2,leader='张三')
+        # one_obj = Projects.objects.filter(Q(id=2)| Q(leader='leo'))
+
+        # one_obj = Projects.objects.filter(desc__contains='描述')
+        # one_obj = one_obj.filter(leader='leo').first().desc
+
+        # one_obj = Projects.objects.get(id=1)
+        # one_obj.leader='肖飞大神'
+        # one_obj.save()
+
+        # one_obj =Projects.objects.filter(name__contains='1')
+        # one_obj1 = one_obj.first()
+        # one_obj1.delete()
+
+        one_obj = Projects.objects.filter(id__gte=1).order_by('programer')
 
         return HttpResponse(one_obj)
         # return HttpResponse('这是一个get请求,pk是'+str(pk))
